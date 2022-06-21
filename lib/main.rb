36ddf3c7023a9b91f23e66432b55905ddfb9fa1e
Dane Dalton <danedalton@sbcglobal.net>
2022-06-21 13:02:27 -0500

    Computer makes a totally random guess for now

--- lib/main.rb
+++ lib/main.rb
@@ -12,6 +12,7 @@ class Game
     @free_colors = COLOR_OPTIONS.clone
     @code = []
     @code_length = 4
+    @guess = nil
     @human_player = Human.new
     @computer_player = Computer.new
     @code_breaker = nil
@@ -23,40 +24,41 @@ class Game
     @human_player.choose_name
     choose_role
     choose_code(self.coder)
-    p self.code
+    code_guessing(self.code_breaker)
+    p self.guess
 
-  end
-
-  def choose_code(player)
-    if player.name == "Computer"
-      set_code_computer
-    else
-      set_code_human
-    end
-  end
-
-  def choose_role
-    invalid = true
-    while invalid
-      invalid = false
-      puts "Would you like to be the code breaker? (y/n)"
-      input = gets.chomp.downcase
-      unless input == "y" || input == "n"
-        Game.invalid_input
-        invalid = true
-      end
-    end
-    if input == "y"
-      self.code_breaker = @human_player
-      self.coder = @computer_player
-    else
-      self.code_breaker = @computer_player
-      self.coder = @human_player
-    end
   end
 
   private
-    attr_accessor :code
+    attr_accessor :code, :guess
+
+    def choose_role
+      invalid = true
+      while invalid
+        invalid = false
+        puts "Would you like to be the code breaker? (y/n)"
+        input = gets.chomp.downcase
+        unless input == "y" || input == "n"
+          Game.invalid_input
+          invalid = true
+        end
+      end
+      if input == "y"
+        self.code_breaker = @human_player
+        self.coder = @computer_player
+      else
+        self.code_breaker = @computer_player
+        self.coder = @human_player
+      end
+    end
+
+    def choose_code(player)
+      if player.name == "Computer"
+        set_code_computer
+      else
+        set_code_human
+      end
+    end
 
     def set_code_computer
       free_color_counter = 6
@@ -80,6 +82,26 @@ class Game
           Game.invalid_input if invalid == true
         end
         self.code.push(self.free_colors.delete(color))
+      end
+    end
+
+    def code_guessing(player)
+      if player.name == "Computer"
+        set_guess_computer
+      else
+        set_guess_human
+      end
+    end
+
+    #Computer takes random guesses for now
+    def set_guess_computer
+      self.free_colors = COLOR_OPTIONS.clone
+      self.guess = []
+      free_color_counter = 6
+      self.code_length.times do
+        color_pick = rand(free_color_counter)
+        self.guess.push(self.free_colors.delete_at(color_pick))
+        free_color_counter -= 1
       end
     end
 
